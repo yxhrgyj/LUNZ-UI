aa99b378591b81fcbd718d8c9ad727a30bae0541
leihg <13589299411@163.com>
2018-01-31 17:37:14 +0800

    history access module sort, delete, delete all

--- src/app/core/shell/header/top-menu/actions/actions.component.ts
+++ src/app/core/shell/header/top-menu/actions/actions.component.ts
@@ -14,42 +14,112 @@ export class ActionsComponent implements OnInit {
     ) {
         const list: any = [
             {
-                id: 1,
+                clickNum: 5,
                 name: `订单管理`,
                 icon: `flaticon-time-1`,
                 url: `/orders`
             },
             {
-                id: 2,
+                clickNum: 10,
                 name: `用户管理`,
                 icon: `flaticon-list-1`,
                 url: `/orders`
             },
             {
-                id: 3,
+                clickNum: 11,
+                name: `用4524户管理`,
+                icon: `flaticon-list-1`,
+                url: `/orders`
+            },
+            {
+                clickNum: 12,
+                name: `用户12管理`,
+                icon: `flaticon-list-1`,
+                url: `/orders`
+            },
+            {
+                clickNum: 17,
+                name: `用户12管理`,
+                icon: `flaticon-list-1`,
+                url: `/orders`
+            },
+            {
+                clickNum: 18,
+                name: `用户1286管理`,
+                icon: `flaticon-list-1`,
+                url: `/orders`
+            },
+            {
+                clickNum: 3,
                 name: `角色管理`,
                 icon: `flaticon-lifebuoy`,
                 url: `/orders`
             }
         ];
         localStorage.setItem(`openModelList`, JSON.stringify(list));
-    }
+    };
 
     ngOnInit() {
         this.getOpenModeList();
-    }
+    };
+
+    sortModelList(modeList: any): void {
+        modeList.sort((x: any, y: any) => {
+            return y.clickNum - x.clickNum;
+        });
+    };
+
     getOpenModeList(): void {
         const modeList = JSON.parse(localStorage.getItem(`openModelList`));
+
         if (modeList !== null) {
+            this.sortModelList(modeList);
             this.openModelList = modeList;
-        };
+        }
     };
-    jumpMode(row: any) {
+
+    fastEntryModule(row: any): void {
         const url = row.url;
         this.router.navigate([url]);
     };
-    deleteModel(i: number) {
+
+    moveModel(row: any, i: any, sortType: number): void {
+        if (sortType === 1) {
+            const rowClickNum = row.clickNum;
+            const rowIndex = i;
+
+            this.openModelList.forEach((item, index) => {
+                if (i === index + 1) {
+                    const preRowClickNum = item.clickNum;
+                    row.clickNum = preRowClickNum + 1;
+                }
+            });
+
+        } else if (sortType === -1) {
+            const rowClickNum = row.clickNum;
+            const rowIndex = i;
+
+            this.openModelList.forEach((item, index) => {
+                if (i === index - 1) {
+                    const preRowClickNum = item.clickNum;
+                    row.clickNum = preRowClickNum - 1;
+                }
+            });
+        }
+
+        this.sortModelList(this.openModelList);
+        localStorage.setItem(`openModelList`, JSON.stringify(this.openModelList));
+    };
+
+    deleteModel(i: number): void {
         this.openModelList.splice(i, 1);
+        localStorage.setItem(`openModelList`, JSON.stringify(this.openModelList));
+    };
+
+    deleteModelAll(): void {
+        const count = this.openModelList.length;
+        this.openModelList.splice(0, count);
+        localStorage.setItem(`openModelList`, JSON.stringify(this.openModelList));
     };
 
 };

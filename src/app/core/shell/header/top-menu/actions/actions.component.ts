b3bee8bbb57fe36b3c1fb92aaec98cb300dac76b
leihg <13589299411@163.com>
2018-01-31 15:18:43 +0800

    delete hstory module

--- src/app/core/shell/header/top-menu/actions/actions.component.ts
+++ src/app/core/shell/header/top-menu/actions/actions.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-top-menu-actions, [app-top-menu-actions]',
-  templateUrl: './actions.component.html',
-  styleUrls: ['./actions.component.scss']
+    selector: 'app-top-menu-actions, [app-top-menu-actions]',
+    templateUrl: './actions.component.html',
+    styleUrls: ['./actions.component.scss']
 })
 export class ActionsComponent implements OnInit {
+    openModelList: Array<any> = [];
+    editModel: Boolean = false;
+    constructor(
+        private router: Router
+    ) {
+        const list: any = [
+            {
+                id: 1,
+                name: `订单管理`,
+                icon: `flaticon-time-1`,
+                url: `/orders`
+            },
+            {
+                id: 2,
+                name: `用户管理`,
+                icon: `flaticon-list-1`,
+                url: `/orders`
+            },
+            {
+                id: 3,
+                name: `角色管理`,
+                icon: `flaticon-lifebuoy`,
+                url: `/orders`
+            }
+        ];
+        localStorage.setItem(`openModelList`, JSON.stringify(list));
+    }
 
-  constructor() { }
+    ngOnInit() {
+        this.getOpenModeList();
+    }
+    getOpenModeList(): void {
+        const modeList = JSON.parse(localStorage.getItem(`openModelList`));
+        if (modeList !== null) {
+            this.openModelList = modeList;
+        };
+    };
+    jumpMode(row: any) {
+        const url = row.url;
+        this.router.navigate([url]);
+    };
+    deleteModel(i: number) {
+        this.openModelList.splice(i, 1);
+    };
 
-  ngOnInit() {
-  }
-
-}
+};

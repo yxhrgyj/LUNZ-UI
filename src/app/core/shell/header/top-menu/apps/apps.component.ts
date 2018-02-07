70bec966e62e0fa07740f9b4ba46ebe04e4eef77
leihg <13589299411@163.com>
2018-02-07 14:58:31 +0800

    optimization

--- src/app/core/shell/header/top-menu/apps/apps.component.ts
+++ src/app/core/shell/header/top-menu/apps/apps.component.ts
@@ -7,53 +7,50 @@ import { AuthenticationService } from '../../../../../core/authentication/authen
 import { ProfileService } from '../../../../profile/profile.service';
 
 @Component({
-  selector: 'app-top-menu-apps, [app-top-menu-apps]',
-  templateUrl: './apps.component.html',
-  styleUrls: ['./apps.component.scss']
+    selector: 'app-top-menu-apps, [app-top-menu-apps]',
+    templateUrl: './apps.component.html',
+    styleUrls: ['./apps.component.scss']
 })
+
 export class AppsComponent implements OnInit {
+    log: Logger;
+    categories: Array<any> = [];
+    get width(): any {
+        return this.categories.length < 2 ? 'auto' : this.categories.length * 250;
+    };
 
-  log: Logger;
-  categories: Array<any> = [];
-  get width(): any {
-    return this.categories.length < 2 ? 'auto' : this.categories.length * 250;
-  }
+    constructor(
+        private authenticationService: AuthenticationService,
+        private profileService: ProfileService,
+        private loggerFactory: LoggerFactory) {
 
-  constructor(
-    private authenticationService: AuthenticationService,
-    private profileService: ProfileService,
-    private loggerFactory: LoggerFactory) {
+        this.log = this.loggerFactory.getLogger();
 
-    this.log = this.loggerFactory.getLogger();
+    };
 
-  }
+    ngOnInit() {
+        const isAuthenticated: boolean = this.authenticationService.isAuthenticated();
 
-  ngOnInit() {
+        if (isAuthenticated) {
+            this.getApps();
+        }
+    };
 
-    const isAuthenticated: boolean = this.authenticationService.isAuthenticated();
+    hasIcon(url: string): boolean {
+        return (url && url !== '');
+    };
 
-    if (isAuthenticated) {
-      this.getApps();
-    }
+    getIconUrl(url: string): string {
+        return this.hasIcon(url) ? url.replace('~/', environment.userCenter.authUri) : url;
+    };
 
-  }
-
-  hasIcon(url: string): boolean {
-    return (url && url !== '');
-  }
-
-  getIconUrl(url: string): string {
-    return this.hasIcon(url) ? url.replace('~/', environment.userCenter.authUri) : url;
-  }
-
-  private getApps() {
-    this.profileService.getApps()
-      .subscribe(categories => {
-        this.categories = categories;
-        this.log.debug(categories);
-      }, error => {
-        this.log.error(error);
-      });
-  }
-
-}
+    private getApps() {
+        this.profileService.getApps()
+            .subscribe(categories => {
+                this.categories = categories;
+                this.log.debug(categories);
+            }, error => {
+                this.log.error(error);
+            });
+    };
+};
